0aac3f9c10602a46ca9e0c6c4b868d32f157386e
Muhammad <mu.aiatullah@gmail.com>
2023-05-28 10:56:12 +0200

    commit

---
+++ src/main/java/StudentManagementSystem.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+    private String name;
+    private int age;
+    private String rollNumber;
+
+    public Student(String name, int age, String rollNumber) {
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+}
+
+public class StudentManagementSystem {
+    private List<Student> students;
+    private Scanner scanner;
+
+    public StudentManagementSystem() {
+        students = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
+
+    public void addStudent() {
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine(); // consume the newline character
+
+        System.out.print("Enter roll number: ");
+        String rollNumber = scanner.nextLine();
+
+        Student student = new Student(name, age, rollNumber);
+        students.add(student);
+
+        System.out.println("Student added successfully!");
+    }
+
+    public void displayStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students found.");
+        } else {
+            System.out.println("Student List:");
+            for (Student student : students) {
+                System.out.println("Name: " + student.getName());
+                System.out.println("Age: " + student.getAge());
+                System.out.println("Roll Number: " + student.getRollNumber());
+                System.out.println("-------------------------");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        StudentManagementSystem sms = new StudentManagementSystem();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nStudent Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display Students");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume the newline character
+
+            switch (choice) {
+                case 1:
+                    sms.addStudent();
+                    break;
+                case 2:
+                    sms.displayStudents();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
